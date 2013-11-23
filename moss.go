3e20b61682dcd8af4cc0036a7e36effb103170a6
Danilo Bargen <gezuru@gmail.com>
2013-11-23 19:02:44 +0100

    Write loop

--- moss.go
+++ moss.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "net"
 )
 
 // Connection struct
 type Connection struct {
+    // Server connection
     server string
     socket net.Conn
 
+    // User information
     nick, user string
+
+    // Read/write channels
+    chanRead, chanWrite chan string
+
+    // Exit channels
+    chanExit chan bool
 }
 
 // Create a new IRC connection object
@@ -18,6 +27,7 @@ func IRC(nick, user string) *Connection {
     irc := &Connection {
         nick: nick,
         user: user,
+        chanExit: make(chan bool),
     }
     return irc
 }
@@ -25,11 +35,38 @@ func IRC(nick, user string) *Connection {
 // Method to connect to IRC server
 func (irc *Connection) Connect(server string) error {
     var err error
+    irc.server = server
     irc.socket, err = net.Dial("tcp", server)
     if err != nil {
         return err
     }
+    fmt.Printf("Connected to %s (%s)\n", irc.server, irc.socket.RemoteAddr())
+
+    irc.chanRead = make(chan string, 16)
+    irc.chanWrite = make(chan string, 16)
+
+    go irc.readLoop()
+    go irc.writeLoop()
+
     return nil
+}
+
+// Write loop
+func (irc *Connection) writeLoop() {
+    for {
+        data := <-irc.chanWrite
+
+        fmt.Printf("--> %s\n", data)
+        _, err := irc.socket.Write([]byte(data))
+        if err != nil {
+            // TODO log errors
+            break
+        }
+    }
+}
+
+// Read loop
+func (irc *Connection) readLoop() {
 }
 
 // Main loop
@@ -44,4 +81,8 @@ func main() {
     if err != nil {
         panic(err)
     }
+
+    irc.chanWrite <- fmt.Sprintf("JOIN %s\r\n", channel)
+
+    <-irc.chanExit
 }

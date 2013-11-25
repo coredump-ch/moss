4de5c5f8938507fdfec17018f26fa4058d5d8dc0
Danilo Bargen <gezuru@gmail.com>
2013-11-25 01:48:01 +0100

    Removed unneeded print statement

--- plugins/status/plugin_status.go
+++ plugins/status/plugin_status.go
@@ -1,7 +1,6 @@
 package plugin_status
 
 import (
-    "fmt"
     "github.com/thoj/go-ircevent"
 
     "github.com/coredump-ch/moss/conf"
@@ -9,7 +8,6 @@ import (
 )
 
 func printStatus(args string, e *irc.Event, con *irc.Connection) error {
-    fmt.Println(e)
     con.Privmsg(conf.Channel, "Status unknown.")
     return nil
 }

707badc2329f6e0c7d046a611210a9edda0e7ce6
Danilo Bargen <gezuru@gmail.com>
2013-11-25 01:45:19 +0100

    Added BOFH excuses plugin

---
+++ plugins/excuse/plugin_excuse.go
@@ -0,0 +1,24 @@
+// BOFH excuses.
+
+package plugin_excuse
+
+import (
+    "fmt"
+    "github.com/thoj/go-ircevent"
+    "math/rand"
+    "time"
+
+    "github.com/coredump-ch/moss/conf"
+    "github.com/coredump-ch/moss/plugin"
+)
+
+func printExcuse(args string, e *irc.Event, con *irc.Connection) error {
+    rand.Seed(time.Now().Unix())
+    i := rand.Intn(len(excuses))
+    con.Privmsg(conf.Channel, fmt.Sprintf("The problem: %s.", excuses[i]))
+    return nil
+}
+
+func init() {
+    plugin.RegisterCommand("excuse", "PRIVMSG", printExcuse)
+}
